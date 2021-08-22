6a85423d5b702cf6b9e25df06f94348781ccf7d3
jalvinronnie <jalvinronnie@gmail.com>
2021-08-23 02:26:51 +0530

    Few UI corrections

--- src/LeftBar.js
+++ src/LeftBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import ForumIcon from '@material-ui/icons/Forum';
 import EmailIcon from '@material-ui/icons/Email';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import './LeftBar.css';
 import { Box } from '@material-ui/core';
 function LeftBar() {
@@ -21,6 +23,17 @@ function LeftBar() {
 					<EmailIcon />
 				</IconButton>
 			</div>
+			<div className="leftbarDiv">
+				<IconButton className="leftbarButton" color="inherit">
+					<PeopleAltIcon />
+				</IconButton>
+			</div>
+			<div className="leftbarDiv">
+				<IconButton className="leftbarButton" color="inherit">
+					<TrendingUpIcon />
+				</IconButton>
+			</div>
+
 		</Box>
 	);
 }
